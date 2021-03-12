0042ce1e151f1b68a0f1a9365ae407bf5d9367bc
Pavaroti707 <hasicmahir@gmail.com>
2021-03-12 01:32:24 +0100

    Exercises for Day04

---
+++ ex04/src/index.js
@@ -0,0 +1,14 @@
+function main(){
+    // only change code below this line 
+    myDecimal = 8.2; myDecimalE = '254e-5';
+
+    //Only change code above this line 
+    return {
+        myDecimal,
+        myDecimalE
+    };
+}
+
+
+console.log(main());
+module.exports = main;
---
+++ ex10/src/index.js
@@ -0,0 +1,19 @@
+function main(){
+    // only change code below this line 
+    var myFirstName = "Mahir";
+    var myLastName = "Hasic";
+    var myHomeTown = "Sarajevo";
+
+
+    //Only change code above this line 
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+    
+
+
+console.log(main());         
+ module.exports = main; 
